00484850b0f597b1727c70da3f79551510a73be0
Mohamed Ben <b.mohamed@tomba.io>
2025-09-01 18:13:16 +0100

    Enhance Bulk model with additional parameters for search and finder operations, and update response structures to use time.Time for date fields

--- tomba/models/Bulk.go
+++ tomba/models/Bulk.go
@@ -1,5 +1,27 @@
 package models
 
+import (
+	"encoding/json"
+	"time"
+)
+
+type BulkType string
+
+const (
+	BulkTypeSearch         BulkType = "search"
+	BulkTypeSimilar        BulkType = "similar"
+	BulkTypeCompany        BulkType = "company"
+	BulkTypeFinder         BulkType = "finder"
+	BulkTypeEnrich         BulkType = "enrich"
+	BulkTypeLinkedIn       BulkType = "linkedin"
+	BulkTypeAuthor         BulkType = "author"
+	BulkTypeVerifier       BulkType = "verifier"
+	BulkTypePhoneFinder    BulkType = "phone-finder"
+	BulkTypePhoneValidator BulkType = "phone-validator"
+)
+
+// Request Models
+
 // BulkGetParams represents parameters for getting bulk operations
 type BulkGetParams struct {
 	Page      int    `json:"page,omitempty"`
@@ -10,22 +32,34 @@ type BulkGetParams struct {
 
 // BulkCreateParams represents parameters for creating bulk operations
 type BulkCreateParams struct {
-	Name      string `json:"name"`
-	List      string `json:"list,omitempty"`
-	Sources   bool   `json:"sources,omitempty"`
-	Notifie   bool   `json:"notifie,omitempty"`
-	Verify    bool   `json:"verify,omitempty"`
-	Total     int    `json:"total,omitempty"`
-	Delimiter string `json:"delimiter,omitempty"`
-	Valid     bool   `json:"valid,omitempty"`
-	Column    int    `json:"column,omitempty"`
+	Name      string `json:"name" form:"name"`
+	List      string `json:"list,omitempty" form:"list"`
+	Sources   bool   `json:"sources,omitempty" form:"sources"`
+	Notifie   bool   `json:"notifie,omitempty" form:"notifie"`
+	Verify    bool   `json:"verify,omitempty" form:"verify"`
+	Total     int    `json:"total,omitempty" form:"total"`
+	Delimiter string `json:"delimiter,omitempty" form:"delimiter"`
+	Valid     bool   `json:"valid,omitempty" form:"valid"`
+	Column    int    `json:"column,omitempty" form:"column"`
 }
 
 // BulkSearchParams represents parameters specific to search bulk operations
 type BulkSearchParams struct {
-	Maximum    string `json:"maximum"` // "1", "5", "10", "20", "50", "100"
-	EmailType  string `json:"email_type"`
-	Department string `json:"department"`
+	Maximum    string                     `json:"maximum" form:"maximum"` // "1", "5", "10", "20", "50", "100"
+	EmailType  BulkSearchParamsType       `json:"email_type" form:"email_type"`
+	Department BulkSearchParamsDepartment `json:"department" form:"department"`
+}
+type BulkSearchParamsType struct {
+	Type         string `json:"type" form:"type"`                   // "all", "personal", "generic"
+	PriorityType string `json:"priority_type" form:"priority_type"` // "only" or "priority"
+}
+type BulkSearchParamsDepartment struct {
+	Name         []string `json:"name" form:"name"`                   // oneof=all engineering sales finance hr it marketing operations management executive legal support communication software security pr warehouse diversity administrative facilities accounting"
+	PriorityType string   `json:"priority_type" form:"priority_type"` // "only" or "exclude"
+}
+
+func (r *BulkSearchParamsType) BulkSearchParamsTypeMarshal() ([]byte, error) {
+	return json.Marshal(r)
 }
 
 // BulkFinderParams represents parameters specific to finder bulk operations
@@ -66,6 +100,28 @@ type BulkFileUpload struct {
 	FieldName string
 }
 
+// Combined parameter types for specific bulk operations
+type BulkSearchCreateParams struct {
+	BulkCreateParams
+	BulkSearchParams
+}
+
+type BulkFinderCreateParams struct {
+	BulkCreateParams
+	BulkFinderParams
+}
+
+type BulkPhoneValidatorCreateParams struct {
+	BulkCreateParams
+	BulkPhoneValidatorParams
+}
+
+type BulkExportCreateParams struct {
+	BulkCreateParams
+}
+
+// Response Models
+
 // BulkItem represents a bulk operation item
 type BulkItem struct {
 	BulkID      int64       `json:"bulk_id"`
@@ -80,7 +136,7 @@ type BulkItem struct {
 	Status      bool        `json:"status"`
 	Chart       interface{} `json:"chart,omitempty"`
 	Table       interface{} `json:"table,omitempty"`
-	CreatedAt   string      `json:"created_at"`
+	CreatedAt   time.Time   `json:"created_at"`
 	UserID      int64       `json:"user_id"`
 	Launched    *bool       `json:"launched,omitempty"`
 	Used        bool        `json:"used"`
@@ -90,16 +146,17 @@ type BulkItem struct {
 	VerifyCost  *int        `json:"verify_cost,omitempty"`
 	TotalEmails *int        `json:"total_emails,omitempty"`
 	Processed   int         `json:"processed"`
-	ExpiredAt   string      `json:"expired_at"`
+	ExpiredAt   time.Time   `json:"expired_at"`
 	Expired     bool        `json:"expired"`
 	BulkType    string      `json:"bulk_type"`
 }
 
 // BulkProgress represents bulk operation progress
 type BulkProgress struct {
-	Status    bool `json:"status"`
-	Progress  int  `json:"progress"`
-	Processed int  `json:"processed"`
+	Status         bool `json:"status"`
+	Progress       int  `json:"progress"`
+	Processed      int  `json:"processed"`
+	ProcessedEmail int  `json:"processed_email"`
 }
 
 // BulkListResponse represents the response for bulk list operations
