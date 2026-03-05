c786e3653f0e66dea59bf3bb373b324e1a239a03
Mohamed Ben <b.mohamed@tomba.io>
2026-03-05 23:44:44 +0100

    feat: update Finder model to support optional fields and add AuthorInfo struct

--- tomba/models/Finder.go
+++ tomba/models/Finder.go
@@ -13,32 +13,51 @@ func (r *Finder) Marshal() ([]byte, error) {
 }
 
 type Finder struct {
-	Data FinderData `json:"data"`
+	Data interface{} `json:"data,omitempty"` // Can be FinderData or []FinderData
 }
 
 type FinderData struct {
-	WebsiteURL   string             `json:"website_url"`
-	AcceptAll    bool               `json:"accept_all"`
-	Email        string             `json:"email"`
-	FirstName    string             `json:"first_name"`
-	LastName     string             `json:"last_name"`
-	Country      string             `json:"country"`
-	Gender       string             `json:"gender"`
-	PhoneNumber  bool               `json:"phone_number"`
-	Position     string             `json:"position"`
-	Twitter      interface{}        `json:"twitter"`
-	Linkedin     string             `json:"linkedin"`
-	Disposable   bool               `json:"disposable"`
-	Webmail      bool               `json:"webmail"`
-	FullName     string             `json:"full_name"`
-	Company      string             `json:"company"`
-	Score        int64              `json:"score"`
-	Verification FinderVerification `json:"verification"`
-	Sources      []SourceElement    `json:"sources"`
-	PhoneData    []PhoneData        `json:"phone_data,omitempty"`
+	WebsiteURL   string              `json:"website_url,omitempty"`
+	AcceptAll    bool                `json:"accept_all,omitempty"`
+	Email        string              `json:"email,omitempty"`
+	FirstName    string              `json:"first_name,omitempty"`
+	LastName     string              `json:"last_name,omitempty"`
+	FullName     string              `json:"full_name,omitempty"`
+	Country      *string             `json:"country,omitempty"`
+	Gender       string              `json:"gender,omitempty"`
+	PhoneNumber  bool                `json:"phone_number,omitempty"`
+	Position     string              `json:"position,omitempty"`
+	Twitter      *string             `json:"twitter,omitempty"`
+	Linkedin     *string             `json:"linkedin,omitempty"`
+	Disposable   bool                `json:"disposable,omitempty"`
+	Webmail      bool                `json:"webmail,omitempty"`
+	Company      string              `json:"company,omitempty"`
+	Score        int64               `json:"score,omitempty"`
+	Verification *FinderVerification `json:"verification,omitempty"`
+	Sources      []SourceElement     `json:"sources,omitempty"`
+	Info         *AuthorInfo         `json:"info,omitempty"`
 }
 
 type FinderVerification struct {
-	Date   string `json:"date"`
-	Status string `json:"status"`
+	Date   string `json:"date,omitempty"`
+	Status string `json:"status,omitempty"`
+}
+
+type AuthorInfo struct {
+	URL           string   `json:"url,omitempty"`
+	Title         string   `json:"title,omitempty"`
+	OgImage       string   `json:"og_image,omitempty"`
+	Description   string   `json:"description,omitempty"`
+	FullName      string   `json:"full_name,omitempty"`
+	FirstName     string   `json:"first_name,omitempty"`
+	LastName      string   `json:"last_name,omitempty"`
+	ArticleDomain string   `json:"article_domain,omitempty"`
+	SameAsDomain  string   `json:"same_as_domain,omitempty"`
+	Linkedin      string   `json:"linkedin,omitempty"`
+	Twitter       string   `json:"twitter,omitempty"`
+	Email         string   `json:"email,omitempty"`
+	Image         string   `json:"image,omitempty"`
+	Gravatar      bool     `json:"gravatar,omitempty"`
+	Emails        []string `json:"emails,omitempty"`
+	AuthorScore   int      `json:"author_score,omitempty"`
 }
